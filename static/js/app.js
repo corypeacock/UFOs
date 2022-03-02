4948f389146f27c5fba2773d519fefdaf72c5add
corypeacock <cory.peacock@gmail.com>
2022-03-02 10:05:04 -0700

    add table via js to index

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,25 @@ const tableData = data;
 // ref to HTML table using d3
 var tbody = d3.select('tbody');
 
+// creating a function to build table
+function buildTable(data) {
+    // clear data to create fresh table
+    tbody.html("");
+
+    // loop through each object in data
+    // append row and cells for each value
+    data.forEach((dataRow) => {
+
+        // append row to table body
+        let row = tbody.append("tr");
+
+        // loop through each field in dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
+
